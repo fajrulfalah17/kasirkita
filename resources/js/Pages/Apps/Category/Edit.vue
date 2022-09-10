ba2b35694dab6700fb6d95de483eafe31349148a
fajrulfalah17 <fajruljunior45@gmail.com>
2022-09-10 21:18:16 +0700

    init commit add view edit update category

--- resources/js/Pages/Apps/Category/Edit.vue
+++ resources/js/Pages/Apps/Category/Edit.vue
@@ -8,22 +8,22 @@
       <div class="fade-in">
         <div class="row">
           <div class="col-md-12">
-            <div class="card border-0 rounded-2 shadow border-top-purple">
+            <div class="card boirder-0 rounded-2 shadow border-top-purple">
               <div class="card-header">
                 <span class="font-weight-bold"
-                  ><i class="fa fa-adjust"></i> Edit Ukuran</span
+                  ><i class="fa fa-folder"></i> Kategori</span
                 >
               </div>
               <div class="card-body">
                 <form @submit.prevent="submit">
                   <div class="mb-3">
-                    <label class="fw-bold">Nama Ukuran</label>
+                    <label class="fw-bold">Nama Kategori</label>
                     <input
                       class="form-control"
                       v-model="form.name"
                       :class="{ 'is-invalid': errors.name }"
                       type="text"
-                      placeholder="Nama Ukuran"
+                      placeholder="Nama Kategori"
                     />
                   </div>
                   <div v-if="errors.name" class="alert alert-danger">
@@ -32,7 +32,7 @@
 
                   <div class="mb-3">
                     <label class="fw-bold">Deskripsi</label>
-                    <textarea rows="4" placeholder="Deskripsi Ukuran" v-model="form.description" :class="{ 'is-invalid': errors.description }" class="form-control"></textarea>
+                    <textarea rows="4" placeholder="Deskripsi Kategori" v-model="form.description" :class="{ 'is-invalid': errors.description }" class="form-control"></textarea>
                   </div>
                   <div v-if="errors.description" class="alert alert-danger">
                     {{ errors.description }}
@@ -42,7 +42,7 @@
                     <div class="col-12">
                       <button class="btn btn-primary shadow-sm rounded-sm">
                         <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                        Update
+                        Save
                       </button>
                       <button class="btn btn-warning shadow-sm rounded-sm ms-3">
                         <i class="fa fa-ban" aria-hidden="true"></i> Reset
@@ -80,19 +80,19 @@ export default {
 
     props: {
         errors: Object,
-        type: Object,
+        category: Object,
     },
 
     setup(props) {
         // define form with reactive
         const form = reactive({
-            name : props.type.name,
-            description: props.type.description,
+            name : props.category.name,
+            description: props.category.description,
         });
 
         // method submit
         const submit = () => {
-            Inertia.put(`/apps/types/${props.type.id}`, {
+            Inertia.put(`/apps/categories/${props.category.id}`, {
                 // data
                 name: form.name,
                 description: form.description,
